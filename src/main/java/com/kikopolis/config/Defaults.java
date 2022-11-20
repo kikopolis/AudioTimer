63dd12277ff26e2eb6e92343eb5268d32c7daee5
kikopolis <kristo.leas@gmail.com>
2022-11-20 11:28:49 +0200

    app data dir now saves by application configured name

--- src/main/java/com/kikopolis/config/Defaults.java
+++ src/main/java/com/kikopolis/config/Defaults.java
@@ -1,14 +1,15 @@
 package com.kikopolis.config;
 
 import java.io.File;
+import java.util.Objects;
 
 public final class Defaults {
     public static final int WIDTH = 1024;
     public static final int HEIGHT = 768;
-    public static final String DATA_DIR = System.getProperty("user.home") + File.separator + ".kikopolis" + File.separator + "audio_timer";
-    public static final String ICON_PATH = Defaults.class.getResource("/images/icon/icon.png").getPath();
-    public static final String APP_NAME = "Kikopolis";
+    public static final String APP_NAME = "Kikopolis Audio Timer";
     public static final String APP_VERSION = "0.0.1";
+    public static final String ICON_PATH = Objects.requireNonNull(Defaults.class.getResource("/images/icon/icon.png")).getPath();
+    public static final String DATA_DIR = System.getProperty("user.home") + File.separator + ".kikopolis" + File.separator + APP_NAME;
     
     private Defaults() {
     }
